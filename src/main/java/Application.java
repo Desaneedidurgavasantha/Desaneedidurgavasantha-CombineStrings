537cca12d7a90ea067d62e1cdc5029a497a91fee
Desaneedidurgavasantha <dedurga132@gmail.com>
2025-05-14 19:20:55 +0530

    commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     /**
      * This class contains a main method that allows you to manually test the CombineStrings lab functionality.
-     * No changes  affect the test . You must still run & pass the test cases.
+     * No changes test cases.
      * All implementation lass.
      *
      * The Lab.combine method should return the combination of two Strings. (eg "string1"+"string2"="string1string2")
